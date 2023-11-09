d4e7ae54bdebd48d6f06a858e434f26510b8a75e
leyuumm <monleonkhenz@gmail.com>
2023-11-10 01:26:49 +0800

    Timing of Lyrics

--- NeuRosesLyrics.java
+++ NeuRosesLyrics.java
@@ -3,22 +3,51 @@ public class NeuRosesLyrics{
     {
         String[] lyrics = {
             "   There are times I think about that fateful day",
+            "                 ",
             "   I threw your love away",
+            "               ",
             "   Every time I see that look upon your face",
+            "                  ",
             "   The same one that you made",
+            "            ",
             "   When your fragile world was crashing down around you",
+            "              ",
             "   You realized your place",
+            "             ",
             "   And the darkness that you try so hard to subdue",
+            "             ",
             "   It causes you to change",
+            "          ",
             "   But baby, baby, please don't leave me",
+            "          ",
             "   I know I made a big mistake, don't turn my sunshine to shade",
+            "        ",
             "   Baby, please don't leave me",
+            "       ",
             "   I know this is a game we play",
             "   You're always taking my breath away",
+            "                                                         ",
+            "   Should've left you a long time ago",
+            "   All my niggas done told me so",
+            "   But my niggas don't hold me tight",
+            "   When I'm sleeping in my bed at night, only you",
+            "   Should've left you a long time ago",
+            "   All my niggas done told me so",
+            "   But my niggas don't hold me tight",
+            "   When I'm sleeping in my bed at night, only you",
+            "   Babe, I know I fucked up",
+            "   Fucked with some empty cups",
+            "   But you got your friends too",
+            "   But you got your friends too",
+            "   Better believe it, in",
+            "   Truth I'm up on my luck, can't stop running amok",
+            "   But you got your friends too",
+            "   But you got your friends too",
+            "   Better believe it, I.",
         }; // end of lyrics
 
         
-        int characterDelay = 130; // delay between characters in milliseconds
+        int characterDelay = 50; // delay between characters in milliseconds
         int lineDelay = 2000; // de;ay between lines in milliseconds (2 seconds)
 
         System.out.println("============================================================");
